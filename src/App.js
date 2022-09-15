9be25aff7f2740eddc85fb90bf35911b07a840fb
Lucas <Rodriguesdesouzal28@gmail.com>
2022-09-15 11:17:00 -0300

    componentizção principal feita

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Corpo from "./Corpo"
+import NavBar from "./NavBar"
+import SideBar from "./SideBar"
+import Usuario from "./Usuario"
+
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Corpo/>
+            
+            
+                
+                
+
+        </div>
+    )
+}
---
+++ src/Stories.js
@@ -0,0 +1,13 @@
+export default function Stories(props) {
+
+    return (
+        <div class="story">
+            <div class="imagem">
+                <img src={props.imagem} />
+            </div>
+            <div class="usuario">
+                {props.usuario}
+            </div>
+        </div>
+    )
+}
